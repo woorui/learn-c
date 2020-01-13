23f282e1f41996e8d343a5a1143b75d03fd5ffb0
woorui <rui1009479218@qq.com>
2020-01-13 23:04:27 +0800

    temperature

---
+++ chapter1/temperature.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    // float fahr, celsius;
+    // int lower, upper, step;
+
+    // lower = 0;
+    // upper = 300;
+    // step = 20;
+
+    // 1-3 打印标题
+    // fahr = lower;
+    // printf("%13s %16s\n", "华氏温度", "摄氏温度");
+    // while (fahr <= upper)
+    // {
+    //     celsius = (5.0 / 9.0) * (fahr - 32.0);
+
+    //     printf("%8.0f %11.1f\n", fahr, celsius);
+
+    //     fahr = fahr + step;
+    // }
+
+    // 1-4 摄氏温度 -> 华氏温度
+    // float celsius;
+    // for (celsius = 0; celsius <= 200; celsius += 10)
+    // {
+    //     printf("%8.0f %11.1f \n", celsius, (9.0 / 5.0) * celsius + 32);
+    // }
+
+    // 顺序打印
+    // float fahr;
+    // for (fahr = 0; fahr <= 300; fahr += 20)
+    // {
+    //     printf("%8.0f %11.1f \n", fahr, (5.0 / 9.0) * (fahr - 32));
+    // }
+
+    // 1-5 逆序打印
+    // for (fahr = 300; fahr >= 0; fahr -= 20)
+    // {
+    //     printf("%8.0f %11.1f \n", fahr, (5.0 / 9.0) * (fahr - 32));
+    // }
+
+    return 0;
+}
