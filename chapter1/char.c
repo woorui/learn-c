5a332649ca7eb35f4044f2790ba2a511c1c33c69
woorui <rui1009479218@qq.com>
2020-01-13 23:40:22 +0800

    update

---
+++ chapter1/char.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    // int c;
+    // while ((c = getchar()) != EOF)
+    // {
+    //     putchar(c);
+    // }
+
+    // 1-7 打印 EOF
+    printf("EOF = %d", EOF);
+    return 0;
+}
