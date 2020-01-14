22c9c61bef17653e45893f5861486b876c83394f
woorui <rui1009479218@qq.com>
2020-01-14 23:50:10 +0800

    wc

---
+++ chapter1/wc.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+#define IN 1  // 在单词内
+#define OUT 0 // 在单词外
+
+int main(int argc, char const *argv[])
+{
+    // // 统计字符个数，单词个数
+    // int c, nw, nc, state;
+
+    // state = OUT;
+    // nw = nc = 0;
+
+    // while ((c = getchar()) != '\n')
+    // {
+    //     // 字符数 +1
+    //     nc++;
+
+    //     if (c == ' ' || c == '\t')
+    //     {
+    //         state = OUT;
+    //     }
+    //     else if (state == OUT)
+    //     {
+    //         state = IN;
+    //         nw++;
+    //     }
+    // }
+
+    // printf("words count = %d, bytes count = %d \n", nw, nc);
+
+
+    // 1-12 每行打印一个单词
+    int c, nw, nc, state;
+
+    while ((c = getchar()) != '\n')
+    {
+        if (c == ' ' || c == '\t')
+        {
+            printf("\n");
+        }
+        else {
+            putchar(c);
+        }
+    }
+
+    return 0;
+}
