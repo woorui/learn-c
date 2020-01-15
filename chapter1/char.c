dae657996495a9ffc70610b8daa7ff65be1d210a
woorui <rui1009479218@gmail.com>
2020-01-15 10:45:14 +0800

    update

--- chapter1/char.c
+++ chapter1/char.c
@@ -2,13 +2,19 @@
 
 int main(int argc, char const *argv[])
 {
-    // int c;
-    // while ((c = getchar()) != EOF)
-    // {
-    //     putchar(c);
-    // }
-
     // 1-7 打印 EOF
     printf("EOF = %d", EOF);
+
+    // 1-9 替换多个空格为单个空格
+    int c, last;
+    while ((c = getchar()) != EOF)
+    {
+        if (!(c == ' ' && last == ' '))
+        {
+            putchar(c);
+        }
+        last = c;
+    }
+
     return 0;
 }
